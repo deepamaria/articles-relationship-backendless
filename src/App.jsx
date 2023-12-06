15fb807d34504b9a5a39da3acb307d88fcb911b1
Deepa <deepamaria605@gmail.com>
2023-12-06 09:21:46 +0100

    articles component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Backendless from 'backendless';
 import './App.css';
 
 function App() {
+  
+  Backendless.serverURL = "https://eu-api.backendless.com"
+  Backendless.initApp( process.env.REACT_APP_APP_ID, REACT_APP_KEY );
   return (
+    
     <div className="App">
       
     </div>
